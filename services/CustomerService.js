3d5b12fe88e7db0e4a30adba615c98fc04e0585c
AzureAD\ElienMaes <elien.maes@ikologik.com>
2022-02-17 16:33:35 +0100

    Added Customer to api

---
+++ services/CustomerService.js
@@ -0,0 +1,32 @@
+const IkologikApiCredentials = require("../IkologikApiCredentials");
+const AbstractIkologikService = require("./AbstractIkologikService");
+const Search = require("../domain/Search");
+
+const jwtHelper = new IkologikApiCredentials();
+
+class CustomerService extends  AbstractIkologikService{
+    constructor(jwtHelper) {
+        super(jwtHelper);
+    }
+
+    // CRUD actions
+    getUrl(){
+        return `${this.jwtHelper.getUrl()}/api/v2/customer`;
+    }
+
+    getByName(customer, installation, name ){
+        const search = new Search();
+        search.addFilter("name", "EQ", [name]);
+        search.addOrder("name", "ASC");
+
+        // Query
+        const result = this.search(customer, installation, search);
+        if (result && result.length == 1 ){
+            return result[0];
+        }else{
+            return null;
+        }
+    }
+}
+
+module.exports = CustomerService;
